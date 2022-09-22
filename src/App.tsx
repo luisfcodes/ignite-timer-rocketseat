7370742cb2a36a10e0fe0e840f3352534cf6296a
Luis Fernando <luis.silva9902@gmail.com>
2022-09-22 17:31:37 -0300

    Add new components e styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { GlobalStyle } from "./components/styles/global";
-import { defaultTheme } from "./components/styles/themes/default";
+import { Router } from "./Router";
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="danger"/>
-      <Button variant="success"/>
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/components/styles/themes/default.ts
+++
@@ -1,5 +0,0 @@
-export const defaultTheme = {
-  white: '#fff',
-  primary: '#8257e6',
-  secondary: 'orange',
-}
